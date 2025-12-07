e424c6ea3fee3dd8723de1fc2ef8b977f48dc52d
LiJia <lijia.c@gmail.com>
2025-12-07 09:18:36 +0800

    basic skeleton for parse

--- msd-request/src/sql/mod.rs
+++ msd-request/src/sql/mod.rs
@@ -1,8 +1,11 @@
 //! SQL parser for MSD requests.
 //!
 
+use crate::sql::msd_dialect::MsdSqlDialect;
 use crate::{InsertRequest, QueryRequest, RequestError};
 
+use msd_table::Table;
+use sqlparser::ast::Statement;
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
 
@@ -10,16 +13,34 @@ mod msd_dialect;
 
 #[derive(Debug)]
 pub enum SqlRequest {
-  Insert(InsertRequest),
   Query(QueryRequest),
+  CreateTable(String, Table),
+  Insert(InsertRequest),
 }
 
-pub fn parse_sql_to_request(sql: &str) -> Result<SqlRequest, RequestError> {
-  let dialect = GenericDialect {}; // or AnsiDialect
-
+pub fn parse_sql_to_request(sql: &str) -> Result<Vec<SqlRequest>, RequestError> {
+  let dialect = MsdSqlDialect {};
   let ast = Parser::parse_sql(&dialect, sql)?;
 
-  Err(RequestError::UnsupportedSqlStatement)
+  ast.into_iter().filter_map(parse_stmt).collect()
+}
+
+fn parse_stmt(stmt: Statement) -> Option<Result<SqlRequest, RequestError>> {
+  match stmt {
+    Statement::Insert { .. } => {
+      //TODO: parse insert statement to InsertRequest
+      todo!()
+    }
+    Statement::Query { .. } => {
+      //TODO: parse insert statement to InsertRequest
+      todo!()
+    }
+    Statement::CreateTable { .. } => {
+      //TODO: parse create table statement to CreateTable
+      todo!()
+    }
+    _ => Err(RequestError::UnsupportedSqlStatement).into(),
+  }
 }
 
 #[cfg(test)]
