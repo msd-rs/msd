bd08d55abd7df8556381d3ed11a3db998bc85c45
LiJia <lijia.c@gmail.com>
2026-01-07 19:12:43 +0800

    update dir

--- tests/scripts/wheels.py
+++ tests/scripts/wheels.py
@@ -5,9 +5,9 @@ import glob
 def main():
   # argv[1] be like "v0.1.10", by git tag
   version = sys.argv[1][1:]
-  pattern = f"msd-{version}*.whl"
-  target = "./target/wheels/"
-  wheels = glob.glob(pattern, root_dir=target)
+  target = "./target"
+  pattern = f"{target}/wheels/msd-{version}*.whl"
+  wheels = glob.glob(pattern)
 
   print(f"MSD_WHEELS={','.join(wheels)}")
 
